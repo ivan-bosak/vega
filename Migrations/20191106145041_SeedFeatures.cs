3c2aa09157b427899900698636c4b5e184673135
ivan-bosak <ivan.bosak@hotmail.com>
2019-11-07 23:25:02 +0200

    Implementing features

---
+++ Migrations/20191106145041_SeedFeatures.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace vega.Migrations
+{
+    public partial class SeedFeatures : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Features (Name) VALUES ('Hatchback')");
+            migrationBuilder.Sql("INSERT INTO Features (Name) VALUES ('Sedan')");
+            migrationBuilder.Sql("INSERT INTO Features (Name) VALUES ('Crossover')");
+            migrationBuilder.Sql("INSERT INTO Features (Name) VALUES ('Coupe')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Features");
+        }
+    }
+}
